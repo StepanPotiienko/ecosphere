46b3028315b042e8136f18f72e56fb99956254e6
Stepan Potiienko <stepanpotiienko@MacBook-Air-Stepan.local>
2025-05-16 19:50:17 +0300

    init

---
+++ ecosphere/ecosphere/ecosphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecosphereApp.swift
+//  ecosphere
+//
+//  Created by Stepan Potiienko on 16.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ecosphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
